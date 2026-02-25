1cb7dfdb1401f26cb624eda90d5e9df72781ae6e
GeWuYou <95328647+GeWuYou@users.noreply.github.com>
2026-02-25 15:09:51 +0800

    feat(functional): 添加函数式编程支持和Result类型实现
    - 实现了Result和Result<T>结构体，提供函数式错误处理
    - 添加了AsyncExtensions中的函数式编程命名空间引用
    - 在FunctionExtensions中添加函数式编程相关引用
    - 从项目文件中移除LanguageExt.Core依赖包
    - 重构GlobalUsings.cs移除不必要的全局引用
    - 添加ResultExtensions扩展方法支持函数式操作
    - 实现Map、Bind、Match等核心函数式编程方法
    - 添加异步操作支持和安全执行委托功能

--- GFramework.Core/GlobalUsings.cs
+++ GFramework.Core/GlobalUsings.cs
@@ -16,6 +16,3 @@ global using System.Collections.Generic;
 global using System.Linq;
 global using System.Threading;
 global using System.Threading.Tasks;
-global using LanguageExt.Common;
-global using LanguageExt.Effects;
-global using LanguageExt.Pretty;
