28ce315d291ff53a8cdddcf052cb96b4a61bcb2c
GeWuYou <95328647+GeWuYou@users.noreply.github.com>
2026-02-26 11:20:11 +0800

    feat(functional): 添加函数式编程扩展方法
    - 在 ControlExtensions 中添加 TakeIfValue 和 TakeUnlessValue 方法，支持值类型的条件判断
    - 在 ControlExtensions 中添加 When 方法，支持条件执行操作
    - 在 ControlExtensions 中添加 RepeatUntil 方法，支持重复执行直到条件满足
    - 在 ControlExtensions 中添加 Retry 方法，支持同步重试机制
    - 在 FunctionExtensions 中添加 Compose 和 AndThen 方法，支持函数组合
    - 在 FunctionExtensions 中添加 Curry 和 Uncurry 方法，支持函数柯里化
    - 在 FunctionExtensions 中添加 Defer 和 Once 方法，支持延迟执行和单次执行
    - 在 PipeExtensions 中添加 Tap 方法，作为 Also 的别名
    - 在 PipeExtensions 中添加 Pipe 方法，支持管道操作
    - 在 PipeExtensions 中添加 Let 方法，支持作用域函数
    - 在 PipeExtensions 中添加 PipeIf 方法，支持条件管道操作

--- GFramework.Core/functional/functions/FunctionExtensions.cs
+++ GFramework.Core/functional/functions/FunctionExtensions.cs
@@ -99,4 +99,185 @@ public static class FunctionExtensions
     }
 
     #endregion
+
+    #region Compose & AndThen
+
+    /// <summary>
+    ///     Compose：函数组合，返回 f(g(x))
+    ///     数学表示：(f ∘ g)(x) = f(g(x))
+    /// </summary>
+    /// <typeparam name="T">输入类型</typeparam>
+    /// <typeparam name="TIntermediate">中间类型</typeparam>
+    /// <typeparam name="TResult">输出类型</typeparam>
+    /// <param name="f">外层函数</param>
+    /// <param name="g">内层函数</param>
+    /// <returns>组合后的函数</returns>
+    /// <exception cref="ArgumentNullException">当 f 或 g 为 null 时抛出</exception>
+    /// <example>
+    /// <code>
+    /// Func&lt;int, int&gt; addOne = x => x + 1;
+    /// Func&lt;int, int&gt; multiplyTwo = x => x * 2;
+    /// var composed = multiplyTwo.Compose(addOne); // (x + 1) * 2
+    /// var result = composed(5); // (5 + 1) * 2 = 12
+    /// </code>
+    /// </example>
+    public static Func<T, TResult> Compose<T, TIntermediate, TResult>(
+        this Func<TIntermediate, TResult> f,
+        Func<T, TIntermediate> g)
+    {
+        ArgumentNullException.ThrowIfNull(f);
+        ArgumentNullException.ThrowIfNull(g);
+        return x => f(g(x));
+    }
+
+    /// <summary>
+    ///     AndThen：函数链式调用，返回 g(f(x))
+    ///     数学表示：(f >> g)(x) = g(f(x))
+    /// </summary>
+    /// <typeparam name="T">输入类型</typeparam>
+    /// <typeparam name="TIntermediate">中间类型</typeparam>
+    /// <typeparam name="TResult">输出类型</typeparam>
+    /// <param name="f">第一个函数</param>
+    /// <param name="g">第二个函数</param>
+    /// <returns>链式调用后的函数</returns>
+    /// <exception cref="ArgumentNullException">当 f 或 g 为 null 时抛出</exception>
+    /// <example>
+    /// <code>
+    /// Func&lt;int, int&gt; addOne = x => x + 1;
+    /// Func&lt;int, int&gt; multiplyTwo = x => x * 2;
+    /// var chained = addOne.AndThen(multiplyTwo); // (x + 1) * 2
+    /// var result = chained(5); // (5 + 1) * 2 = 12
+    /// </code>
+    /// </example>
+    public static Func<T, TResult> AndThen<T, TIntermediate, TResult>(
+        this Func<T, TIntermediate> f,
+        Func<TIntermediate, TResult> g)
+    {
+        ArgumentNullException.ThrowIfNull(f);
+        ArgumentNullException.ThrowIfNull(g);
+        return x => g(f(x));
+    }
+
+    #endregion
+
+    #region Curry & Uncurry
+
+    /// <summary>
+    ///     Curry：将二参数函数柯里化为嵌套的单参数函数
+    /// </summary>
+    /// <typeparam name="T1">第一个参数类型</typeparam>
+    /// <typeparam name="T2">第二个参数类型</typeparam>
+    /// <typeparam name="TResult">返回值类型</typeparam>
+    /// <param name="func">要柯里化的函数</param>
+    /// <returns>柯里化后的函数</returns>
+    /// <exception cref="ArgumentNullException">当 func 为 null 时抛出</exception>
+    /// <example>
+    /// <code>
+    /// Func&lt;int, int, int&gt; add = (x, y) => x + y;
+    /// var curriedAdd = add.Curry();
+    /// var add5 = curriedAdd(5);
+    /// var result = add5(3); // 8
+    /// </code>
+    /// </example>
+    public static Func<T1, Func<T2, TResult>> Curry<T1, T2, TResult>(
+        this Func<T1, T2, TResult> func)
+    {
+        ArgumentNullException.ThrowIfNull(func);
+        return arg1 => arg2 => func(arg1, arg2);
+    }
+
+    /// <summary>
+    ///     Curry：将三参数函数柯里化为嵌套的单参数函数
+    /// </summary>
+    /// <typeparam name="T1">第一个参数类型</typeparam>
+    /// <typeparam name="T2">第二个参数类型</typeparam>
+    /// <typeparam name="T3">第三个参数类型</typeparam>
+    /// <typeparam name="TResult">返回值类型</typeparam>
+    /// <param name="func">要柯里化的函数</param>
+    /// <returns>柯里化后的函数</returns>
+    /// <exception cref="ArgumentNullException">当 func 为 null 时抛出</exception>
+    /// <example>
+    /// <code>
+    /// Func&lt;int, int, int, int&gt; add3 = (x, y, z) => x + y + z;
+    /// var curriedAdd = add3.Curry();
+    /// var result = curriedAdd(1)(2)(3); // 6
+    /// </code>
+    /// </example>
+    public static Func<T1, Func<T2, Func<T3, TResult>>> Curry<T1, T2, T3, TResult>(
+        this Func<T1, T2, T3, TResult> func)
+    {
+        ArgumentNullException.ThrowIfNull(func);
+        return arg1 => arg2 => arg3 => func(arg1, arg2, arg3);
+    }
+
+    /// <summary>
+    ///     Uncurry：将柯里化的函数还原为多参数函数
+    /// </summary>
+    /// <typeparam name="T1">第一个参数类型</typeparam>
+    /// <typeparam name="T2">第二个参数类型</typeparam>
+    /// <typeparam name="TResult">返回值类型</typeparam>
+    /// <param name="func">柯里化的函数</param>
+    /// <returns>还原后的多参数函数</returns>
+    /// <exception cref="ArgumentNullException">当 func 为 null 时抛出</exception>
+    /// <example>
+    /// <code>
+    /// Func&lt;int, Func&lt;int, int&gt;&gt; curriedAdd = x => y => x + y;
+    /// var add = curriedAdd.Uncurry();
+    /// var result = add(5, 3); // 8
+    /// </code>
+    /// </example>
+    public static Func<T1, T2, TResult> Uncurry<T1, T2, TResult>(
+        this Func<T1, Func<T2, TResult>> func)
+    {
+        ArgumentNullException.ThrowIfNull(func);
+        return (arg1, arg2) => func(arg1)(arg2);
+    }
+
+    #endregion
+
+    #region Defer & Once
+
+    /// <summary>
+    ///     Defer：延迟执行函数，返回 Lazy&lt;T&gt;
+    /// </summary>
+    /// <typeparam name="TResult">返回值类型</typeparam>
+    /// <param name="func">要延迟执行的函数</param>
+    /// <returns>包装了延迟执行的 Lazy 对象</returns>
+    /// <exception cref="ArgumentNullException">当 func 为 null 时抛出</exception>
+    /// <example>
+    /// <code>
+    /// var lazy = (() => ExpensiveComputation()).Defer();
+    /// // 此时尚未执行
+    /// var result = lazy.Value; // 首次访问时才执行
+    /// </code>
+    /// </example>
+    public static Lazy<TResult> Defer<TResult>(this Func<TResult> func)
+    {
+        ArgumentNullException.ThrowIfNull(func);
+        return new Lazy<TResult>(func);
+    }
+
+    /// <summary>
+    ///     Once：确保函数只执行一次，后续调用返回缓存的结果（线程安全）
+    /// </summary>
+    /// <typeparam name="TResult">返回值类型</typeparam>
+    /// <param name="func">要执行的函数</param>
+    /// <returns>包装后的函数，确保只执行一次</returns>
+    /// <exception cref="ArgumentNullException">当 func 为 null 时抛出</exception>
+    /// <example>
+    /// <code>
+    /// var counter = 0;
+    /// var once = (() => ++counter).Once();
+    /// var result1 = once(); // 1
+    /// var result2 = once(); // 1 (不会再次执行)
+    /// </code>
+    /// </example>
+    public static Func<TResult> Once<TResult>(this Func<TResult> func)
+    {
+        ArgumentNullException.ThrowIfNull(func);
+        var lazy = new Lazy<TResult>(func, LazyThreadSafetyMode.ExecutionAndPublication);
+        return () => lazy.Value;
+    }
+
+    #endregion
 }
