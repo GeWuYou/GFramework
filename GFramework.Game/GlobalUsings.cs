bfd068be27c83cfe5c523a5f2113cffe24f6ab05
GeWuYou <95328647+GeWuYou@users.noreply.github.com>
2026-03-03 22:05:18 +0800

    chore(dependencies): 移除 LanguageExt 相关依赖和引用
    - 从 GFramework.Game 项目中移除 LanguageExt.Core 包引用
    - 从 GFramework.Godot 项目中移除 LanguageExt.Core 包引用
    - 从全局引用文件中移除 LanguageExt.Common 引用
    - 从全局引用文件中移除 LanguageExt.Effects 引用
    - 从全局引用文件中移除 LanguageExt.Pretty 引用
    - 保留必要的 System.Threading.Tasks 引用

--- GFramework.Game/GlobalUsings.cs
+++ GFramework.Game/GlobalUsings.cs
@@ -17,6 +17,3 @@ global using System.Collections.Concurrent;
 global using System.Linq;
 global using System.Threading;
 global using System.Threading.Tasks;
-global using LanguageExt.Common;
-global using LanguageExt.Effects;
-global using LanguageExt.Pretty;
