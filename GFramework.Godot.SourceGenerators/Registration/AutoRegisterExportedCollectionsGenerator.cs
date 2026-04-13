ca1214f47f017e9091089f0c0ae4391d64a2a072
GeWuYou <95328647+GeWuYou@users.noreply.github.com>
2026-04-13 10:59:26 +0800

    refactor(generators): 添加通用扩展方法引用
    - 在 AutoSceneGenerator 中引入 GFramework.SourceGenerators.Common.Extensions
    - 在 AutoRegisterExportedCollectionsGenerator 中引入 GFramework.SourceGenerators.Common.Extensions

--- GFramework.Godot.SourceGenerators/Registration/AutoRegisterExportedCollectionsGenerator.cs
+++ GFramework.Godot.SourceGenerators/Registration/AutoRegisterExportedCollectionsGenerator.cs
@@ -1,6 +1,7 @@
 using GFramework.Godot.SourceGenerators.Diagnostics;
 using GFramework.SourceGenerators.Common.Constants;
 using GFramework.SourceGenerators.Common.Diagnostics;
+using GFramework.SourceGenerators.Common.Extensions;
 
 namespace GFramework.Godot.SourceGenerators.Registration;
 
